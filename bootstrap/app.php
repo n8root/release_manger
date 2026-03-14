ac06c4b5a98a24cda0097d56e71f9ec32992423d
Kirill Gladkih <kirillgladkih@atomicmail.io>
2026-03-14 20:14:45 +0500

    init

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,14 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$basePath = dirname(__DIR__);
-$app = new Application($basePath);
-
-return (new \Illuminate\Foundation\Configuration\ApplicationBuilder($app))
-    ->withKernels()
-    ->withEvents()
-    ->withCommands()
-    ->withProviders()
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
